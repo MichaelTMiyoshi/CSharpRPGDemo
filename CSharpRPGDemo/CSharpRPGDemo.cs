5fca08478df2ea204d4a27e52032e6871505fdf2
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-29 15:15:56 -0700

    Add project files.

---
+++ CSharpRPGDemo/CSharpRPGDemo.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpRPGDemo
+{
+    internal class CSharpRPGDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the RPG demo solution!");
+        }
+    }
+}
