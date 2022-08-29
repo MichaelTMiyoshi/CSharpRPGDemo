6d321d9841003784bba96284b3325f2f53300fd4
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-29 15:30:04 -0700

    08/29/2022 Created intro.

--- CSharpRPGDemo/CSharpRPGDemo.cs
+++ CSharpRPGDemo/CSharpRPGDemo.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the RPG demo solution!");
+            Console.WriteLine("You will eventually wander around a 25 room maze");
+            Console.WriteLine("interacting with Non-Player Characters (NPCs).");
+            Console.WriteLine("\n\nHave fun!");
         }
     }
 }
