3a4418b990b605e814eceea627b001d20a4af22e
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-09-05 12:40:00 -0700

    09/05/2022 Looping and branching added.
    Added equipment loop.
    Added game loop.
    All branching and looping have error checking.
    The inputs check upper and lower case letters in the first switch.  After that, the input chars are changed to uppercase.  That is done in two different ways.  Once the input is changed to uppercase in a separate command.  The other times, the char is converted to uppercase on the same line of code that gets the input and converts it from a String to a char.

--- CSharpRPGDemo/CSharpRPGDemo.cs
+++ CSharpRPGDemo/CSharpRPGDemo.cs
@@ -4,12 +4,16 @@
  *              Create Variables for health, damage, name
  *              Max and min for each
  *              Address user by name
+ *              Create equipment loop
+ *              Create game loop
  *  Notes:      The different parts of the RPG will be 
  *              in different branches on GitHub
  *  Maintenance Log:
  *      Date:       Done:
  *      09/05/2022  Introduction done
  *      09/05/2022  Variables
+ *      09/05/2022  Equipment loop and game loop added
+ *                  Both loops and all the branching have error checking
  */
 using System;
 namespace CSharpRPGDemo
@@ -30,10 +34,129 @@ namespace CSharpRPGDemo
             int damage = rng.Next(minStart, maxStart + 1);
             Console.Write("What is your name? >> ");
             String name = Console.ReadLine();
+            bool ready = false;
+            int equipment = 0;
+            String equipmentName = "";
 
+            // equip loop
+            do
+            {
+                Console.WriteLine(name + ", you may choose one item.");
+                Console.WriteLine("\tA. Pencil");
+                Console.WriteLine("\tB. Laptop computer");
+                Console.WriteLine("\tC. Book of matches");
+                Console.Write("\t>> ");
+                char choice = Convert.ToChar(Console.ReadLine());
+                ready = true;
+
+                switch(choice)
+                {
+                    case 'A':
+                    case 'a':
+                        equipment = 1;
+                        equipmentName = "pencil";
+                        break;
+                    case 'B':
+                    case 'b':
+                        equipment = 2;
+                        equipmentName = "laptop computer";
+                        break;
+                    case 'C':
+                    case 'c':
+                        equipment = 3;
+                        equipmentName = "book of matches";
+                        break;
+                    default:
+                        Console.WriteLine("You must choose something.");
+                        ready = false;
+                        break;
+                }
+
+                if(equipment != 0)
+                {
+                    Console.WriteLine("A " + equipmentName + " is a great choice");
+                }
+                if (!ready) { continue; }
+
+                Console.Write("\n\nAre you ready to play the game? (Y/N) >> ");
+                choice = Convert.ToChar(Console.ReadLine());
+                choice = Char.ToUpper(choice);
+                if(choice == 'N')
+                {
+                    ready = false;
+                    Console.WriteLine("Okay.  You may choose again.");
+                }
+            } while (!ready);
+
+            Console.WriteLine("Your stats so far:");
             Console.WriteLine(name);
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Damage: " + damage);
-        }
+
+            bool exit = false;
+            int location = 0;
+            int timesThrough = 0;
+
+            do
+            {
+                char choice;
+                if (location == 0)
+                {
+                    Console.WriteLine("This is room " + location);
+                    Console.Write("Would you like to go to the second room? (Y/N) >> ");
+                    choice = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                    if(choice == 'Y')
+                    {
+                        location = 1;
+                    }
+                }
+                if (location == 1)
+                {
+                    Console.WriteLine("This is room " + location);
+                    if (equipment != 1)
+                    {
+                        Console.WriteLine("You found a pencil.");
+                        Console.Write("Would you like to trade your " + equipmentName
+                            + " for a pencil? (Y/N) >> ");
+                        choice = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                        if (choice == 'Y')
+                        {
+                            equipment = 1;
+                        }
+                        else
+                        {
+                            if (1 <= timesThrough)
+                            {
+                                Console.Write("Are you sure you do not want the pencil? (Y/N) >> ");
+                                choice = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                                if (choice == 'N') { continue; }
+                            }
+                        }
+                    }
+
+                    Console.Write("Would you like to go to the third room? (Y/N) >> ");
+                    choice = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                    if (choice == 'Y')
+                    {
+                        location = 2;
+                    }
+                    timesThrough++;
+                }
+                else if (location == 2)
+                {
+                    Console.WriteLine("This is room " + location);
+                    if (equipment != 1)
+                    {
+                        Console.WriteLine("You do not have a pencil.  You must go back to the first room.");
+                        location = 0;
+                    }
+                    else
+                    {
+                        exit = true;
+                    }
+                }
+            } while (!exit);
+            Console.WriteLine("Way to go!  You passed all the competency tests!");
+       }
     }
 }
