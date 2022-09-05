01c10b7e3615ae4de5521cddff110d2eaf5e10cc
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-09-05 11:40:11 -0700

    09/05/2022 Added name, health, damage.
    Health and damage get initialized with same min and max possibles.  Showed two ways to get random integers.

--- CSharpRPGDemo/CSharpRPGDemo.cs
+++ CSharpRPGDemo/CSharpRPGDemo.cs
@@ -1,11 +1,15 @@
 ﻿/*  Name:       Miyoshi
  *  Problem:    RPG
  *  Pseudocode: Output introduction to the screen
+ *              Create Variables for health, damage, name
+ *              Max and min for each
+ *              Address user by name
  *  Notes:      The different parts of the RPG will be 
  *              in different branches on GitHub
  *  Maintenance Log:
  *      Date:       Done:
  *      09/05/2022  Introduction done
+ *      09/05/2022  Variables
  */
 using System;
 namespace CSharpRPGDemo
@@ -18,6 +22,18 @@ namespace CSharpRPGDemo
             Console.WriteLine("You will eventually wander around a 25 room maze");
             Console.WriteLine("interacting with Non-Player Characters (NPCs).");
             Console.WriteLine("\n\nHave fun!");
+
+            Random rng = new Random();
+            int minStart = 10;
+            int maxStart = 20;
+            int health = minStart + rng.Next(maxStart + 1);
+            int damage = rng.Next(minStart, maxStart + 1);
+            Console.Write("What is your name? >> ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine(name);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Damage: " + damage);
         }
     }
 }
